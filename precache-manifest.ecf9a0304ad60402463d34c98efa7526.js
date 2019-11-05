5dd261833b988bcbf3072168f3dab2169d0f9074
Nick Taylor <technktailor@gmail.com>
2019-11-05 22:21:59 +0530

    Updates

---
+++ precache-manifest.ecf9a0304ad60402463d34c98efa7526.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f971d40ea16a6bee8244cecc0ebd37",
+    "url": "/reactlearningapp/index.html"
+  },
+  {
+    "revision": "a95a5b6969f63f48482a",
+    "url": "/reactlearningapp/static/css/main.3cd75bc7.chunk.css"
+  },
+  {
+    "revision": "4643bd3013a39283ce37",
+    "url": "/reactlearningapp/static/js/2.a9e48c7a.chunk.js"
+  },
+  {
+    "revision": "a95a5b6969f63f48482a",
+    "url": "/reactlearningapp/static/js/main.b440df9e.chunk.js"
+  },
+  {
+    "revision": "dc07cad3f1ad85dae4c7",
+    "url": "/reactlearningapp/static/js/runtime-main.8480547a.js"
+  }
+]);
